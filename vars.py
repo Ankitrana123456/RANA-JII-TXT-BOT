791826a012759b3fd4b4dbfc127dc652affea370
Ankitrana123456 <ankitrana73jio@gmail.com>
2025-06-13 21:22:25 +0530

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,9 @@
+# Don't Remove Credit Tg - @Ashiqi_091
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Ashiqi_091
+# Ask Doubt on telegram @BABATILLU
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "23274330"))
+API_HASH = environ.get("API_HASH", "970e2e79779707c56d2b453b3a6eea48")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
